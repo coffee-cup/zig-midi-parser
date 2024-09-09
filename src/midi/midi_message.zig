22803fe2759b85b5adba6cacfe19fab049dd013e
Jake Runzer <jakerunzer@gmail.com>
2024-09-09 00:13:21 -0400

    return lens of midi events

--- src/midi/midi_message.zig
+++ src/midi/midi_message.zig
@@ -15,6 +15,19 @@ pub const MidiEvent = union(enum) {
     pitch_wheel_change: struct { channel: u8, value: u16 },
     meta_event: MetaEvent,
 
+    pub fn len(self: MidiEvent) usize {
+        return switch (self) {
+            .note_off => 3,
+            .note_on => 3,
+            .note_aftertouch => 3,
+            .controller_change => 3,
+            .program_change => 2,
+            .channel_aftertouch => 2,
+            .pitch_wheel_change => 3,
+            .meta_event => |meta_event| meta_event.len(),
+        };
+    }
+
     pub fn parse(bytes: []const u8) !MidiEvent {
         if (bytes.len < 1) return error.InvalidMessage;
 
@@ -87,6 +100,26 @@ pub const MetaEvent = union(enum) {
     time_signature: struct { numerator: u8, denominator: u8, metro: u8, thirty_seconds: u8 },
     key_signature: struct { key: u8, scale: u8 },
     sequencer_specific: struct { data: []const u8 },
+
+    pub fn len(self: MetaEvent) usize {
+        return switch (self) {
+            .sequence_number => 4,
+            .text => |s| s.text.len + 3,
+            .copyright_notice => |s| s.text.len + 3,
+            .sequence_name => |s| s.text.len + 3,
+            .instrument_name => |s| s.text.len + 3,
+            .lyric => |s| s.text.len + 3,
+            .marker => |s| s.text.len + 3,
+            .cue_point => |s| s.text.len + 3,
+            .midi_channel_prefix => 3,
+            .end_of_track => 1,
+            .set_tempo => 5,
+            .smtpe_offset => 7,
+            .time_signature => 6,
+            .key_signature => 4,
+            .sequencer_specific => |s| s.data.len + 3,
+        };
+    }
 
     pub fn parse(bytes: []const u8) !MetaEvent {
         const meta_event_type = bytes[1];
@@ -166,8 +199,6 @@ pub const MetaEvent = union(enum) {
 
             0x7F => {
                 const data_length = try utils.readVariableLengthQuantity(bytes[2..]);
-                std.debug.print("\nDATA LENGTH: {}\n", .{data_length});
-
                 const data = bytes[3 .. 3 + data_length];
                 return .{ .sequencer_specific = .{ .data = data } };
             },
@@ -191,6 +222,7 @@ test "midi channel events" {
         const bytes = [_]u8{ 0x81, 10, 60 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqual(MidiEvent{ .note_off = .{ .channel = 1, .key = 10, .velocity = 60 } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // NoteOn
@@ -244,6 +276,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x00, 125, 125 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqual(MidiEvent{ .meta_event = .{ .sequence_number = .{ .number = 16125 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // Text
@@ -251,6 +284,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x01, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .text = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // CopyrightNotice
@@ -258,6 +292,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x02, 5, 'h', 'e', 'l', 'l', 'o' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .copyright_notice = .{ .text = "hello" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // SequenceName
@@ -265,6 +300,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x03, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .sequence_name = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // InstrumentName
@@ -272,6 +308,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x04, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .instrument_name = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // Lyric
@@ -279,6 +316,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x05, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .lyric = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // Marker
@@ -286,6 +324,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x06, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .marker = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // CuePoint
@@ -293,6 +332,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x07, 4, 'j', 'a', 'k', 'e' };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .cue_point = .{ .text = "jake" } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // MidiChannelPrefix
@@ -300,6 +340,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x20, 4 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .midi_channel_prefix = .{ .channel = 4 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // EndOfTrack
@@ -307,6 +348,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x2F };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .end_of_track = {} } }, message);
+        try testing.expectEqual(1, message.len());
     }
 
     // SetTempo
@@ -314,6 +356,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x51, 0x02, 0x10, 0x00 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .set_tempo = .{ .microseconds_per_quarter_note = 135168 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // SMPTEOffset
@@ -321,6 +364,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x54, 1, 2, 3, 4, 5 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .smtpe_offset = .{ .hour = 1, .minute = 2, .second = 3, .frame = 4, .sub_frame = 5 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // TimeSignature
@@ -328,6 +372,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x58, 123, 234, 242, 12 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .time_signature = .{ .numerator = 123, .denominator = 234, .metro = 242, .thirty_seconds = 12 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // KeySignature
@@ -335,6 +380,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x59, 123, 234 };
         const message = try MidiEvent.parse(&bytes);
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .key_signature = .{ .key = 123, .scale = 234 } } }, message);
+        try testing.expectEqual(bytes.len, message.len());
     }
 
     // SequencerSpecific
@@ -342,192 +388,7 @@ test "midi meta events" {
         const bytes = [_]u8{ 0xFF, 0x7F, 0x04, 0x00, 0x00, 0x00, 0x00, 0xFF };
         const message = try MidiEvent.parse(&bytes);
 
-        std.debug.print("SequencerSpecific: {any}\n", .{message.meta_event});
-
         try testing.expectEqualDeep(MidiEvent{ .meta_event = .{ .sequencer_specific = .{ .data = &[_]u8{ 0x00, 0x00, 0x00, 0x00 } } } }, message);
+        try testing.expectEqual(7, message.len());
     }
-}
-
-//     fn parse_meta_event(status: u8, bytes: []const u8) !MetaEvent {
-//         const meta_event_type = bytes[1];
-
-//         switch (meta_event_type) {
-//             0x00 => return MidiMessage{
-//                 .status = status,
-//                 .event = .{ .meta_event = .{ .sequence_number = .{ .number = (@as(u16, bytes[2]) << 7) | bytes[3] } } },
-//             },
-
-//             0x01 => {
-//                 const text_length = bytes[2];
-//                 const text = bytes[3 .. 3 + text_length];
-//                 return MidiMessage{
-//                     .status = status,
-//                     .event = .{ .meta_event = .{ .text = text } },
-//                 };
-//             },
-
-//             else => return error.Unimplemented,
-//         }
-//     }
-
-//     fn parse_channel(byte: u8) u4 {
-//         return @truncate(byte & 0x0F);
-//     }
-
-//     pub fn channel(self: MidiMessage) u4 {
-//         return parse_channel(self.status);
-//     }
-// };
-
-// pub const MidiStatusOld = enum(u8) {
-//     NoteOff = 0x80,
-//     NoteOn = 0x90,
-//     PolyphonicKeyPressure = 0xA0,
-//     ControlChange = 0xB0,
-//     ProgramChange = 0xC0,
-//     ChannelPressure = 0xD0,
-//     PitchWheelChange = 0xE0,
-//     SystemExclusive = 0xF0,
-//     // ... other system messages ...
-//     Reset = 0xFF,
-
-//     pub fn parse(byte: u8) MidiStatus {
-//         // Zero out the channel bits before comparing
-//         const status_byte = byte & 0xF0;
-//         return switch (status_byte) {
-//             0x80 => .NoteOff,
-//             0x90 => .NoteOn,
-//             0xA0 => .PolyphonicKeyPressure,
-//             0xB0 => .ControlChange,
-//             0xC0 => .ProgramChange,
-//             0xD0 => .ChannelPressure,
-//             0xE0 => .PitchWheelChange,
-//             0xF0 => .SystemExclusive,
-//             0xFF => .Reset,
-//             else => @panic("Unknown MIDI status"),
-//         };
-
-//         return @enumFromInt(byte);
-//     }
-
-//     pub fn channel(self: MidiStatus) u4 {
-//         return @truncate(self.byte() & 0x0F);
-//     }
-// };
-
-// pub const MidiMessage = union(MidiStatus) {
-//     NoteOff: struct { channel: u4, key: u7, velocity: u7 },
-//     NoteOn: struct { channel: u4, key: u7, velocity: u7 },
-//     PolyphonicKeyPressure: struct { channel: u4, key: u7, pressure: u7 },
-//     ControlChange: struct { channel: u4, controller: u7, value: u7 },
-//     ProgramChange: struct { channel: u4, program: u7 },
-//     ChannelPressure: struct { channel: u4, pressure: u7 },
-//     PitchWheelChange: struct { channel: u4, value: u14 },
-//     SystemExclusive: struct { manufacturer_id: u7, data: []const u8 },
-//     // ... other message types ...
-//     Reset: void,
-
-//     pub fn parse(bytes: []const u8) !MidiMessage {
-//         std.debug.print("MIDI message bytes: {any}\n", .{bytes});
-//         std.debug.print("MIDI message length: {}\n", .{bytes.len});
-//         if (bytes.len < 1) return error.InvalidMessage;
-
-//         const status = MidiStatus.fromByte(bytes[0]);
-
-//         std.debug.print("Parsed MIDI status: {}\n", .{status});
-
-//         const channel: u4 = @truncate(bytes[0] & 0x0F);
-
-//         return switch (status) {
-//             .NoteOff => .{
-//                 .NoteOff = .{
-//                     .channel = channel,
-//                     .key = @truncate(bytes[1]),
-//                     .velocity = @truncate(bytes[2]),
-//                 },
-//             },
-
-//             .NoteOn => .{
-//                 .NoteOn = .{
-//                     .channel = channel,
-//                     .key = @truncate(bytes[1]),
-//                     .velocity = @truncate(bytes[2]),
-//                 },
-//             },
-
-//             .PolyphonicKeyPressure => .{
-//                 .PolyphonicKeyPressure = .{
-//                     .channel = channel,
-//                     .key = @truncate(bytes[1]),
-//                     .pressure = @truncate(bytes[2]),
-//                 },
-//             },
-
-//             .ControlChange => .{
-//                 .ControlChange = .{
-//                     .channel = channel,
-//                     .controller = @truncate(bytes[1]),
-//                     .value = @truncate(bytes[2]),
-//                 },
-//             },
-
-//             .ProgramChange => .{
-//                 .ProgramChange = .{
-//                     .channel = channel,
-//                     .program = @truncate(bytes[1]),
-//                 },
-//             },
-
-//             .ChannelPressure => .{
-//                 .ChannelPressure = .{
-//                     .channel = channel,
-//                     .pressure = @truncate(bytes[1]),
-//                 },
-//             },
-
-//             .Reset => .{
-//                 .Reset = {},
-//             },
-
-//             else => error.Unimplemented,
-//         };
-//     }
-// };
-
-test "midi message" {
-    // const testing = std.testing;
-
-    // // Test NoteOn message
-    // {
-    //     const bytes = [_]u8{ 0x90, 0x3C, 0x7F };
-    //     const message = try MidiMessage.parse(&bytes);
-    //     try testing.expectEqual(MidiStatus.NoteOn, @as(MidiStatus, message));
-    //     try testing.expectEqual(@as(u4, 0), message.NoteOn.channel);
-    //     try testing.expectEqual(@as(u7, 60), message.NoteOn.key);
-    //     try testing.expectEqual(@as(u7, 127), message.NoteOn.velocity);
-    // }
-
-    // // Test NoteOff message
-    // {
-    //     const bytes = [_]u8{ 0x80, 0x48, 0x40 };
-    //     const message = try MidiMessage.parse(&bytes);
-    //     try testing.expectEqual(MidiStatus.NoteOff, @as(MidiStatus, message));
-    //     try testing.expectEqual(@as(u4, 0), message.NoteOff.channel);
-    //     try testing.expectEqual(@as(u7, 72), message.NoteOff.key);
-    //     try testing.expectEqual(@as(u7, 64), message.NoteOff.velocity);
-    // }
-
-    // Test invalid message (too short)
-    // {
-    //     const bytes = [_]u8{};
-    //     try testing.expectError(error.InvalidMessage, MidiMessage.parse(&bytes));
-    // }
-
-    // Test unimplemented message type
-    // {
-    //     const bytes = [_]u8{ 0xAB, 0x00, 0x00 };
-
-    //     const result = MidiMessage.parse(&bytes);
-    //     try testing.expectError(error.Unimplemented, result);
-    // }
 }
